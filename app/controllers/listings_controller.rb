f4ed8e29bdf59445be32bd7cbcfc2dec90f553fd
Greg Santulli <gregsantulli@gmail.com>
2015-04-20 19:52:14 -0700

    add welcome and listing controllers, views, models and migrations add gem to geolocate address of each listing.

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,38 @@
+class ListingsController < ApplicationController
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    # binding.pry
+    if @listing.save
+     params[:photos]['photo'].each do |a|
+      @photo = @listing.photos.create!(:photo => a, :listing_id => @listing.id)
+      end
+      redirect_to @listing
+    else
+       # format.html { render action: 'new' }
+       redirect_to '/'
+     end
+   end
+
+   def update
+    listing = Listing.find(params[:id])
+    listing.update_attributes(listing_params)
+    listing.save
+    redirect_to listing_path
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:name, :address, :active, :price)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,32 @@
 Rails.application.routes.draw do
+  # get 'listings/show'
+
+  get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+
+
+
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    get 'listings' => 'welcome#listings'
+
+
+    get 'api/listings' => 'api#listings'
+    get 'api/listings/:id' => 'api#listings'
+
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+    resources :listings
 
   # Example resource route with options:
   #   resources :products do
