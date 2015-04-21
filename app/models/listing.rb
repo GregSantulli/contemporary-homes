7a3c5384965d2b623f6e5eca0545c44664911b71
Greg Santulli <gregsantulli@gmail.com>
2015-04-20 19:59:17 -0700

    add photo uploader using carrierwave gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,6 @@
 class Listing < ActiveRecord::Base
+  geocoded_by :address
+  after_validation :geocode
+  has_many :photos
+  accepts_nested_attributes_for :photos
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420041224) do
+ActiveRecord::Schema.define(version: 20150420231740) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "name"
     t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
     t.boolean  "active"
     t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "listing_id"
+    t.string   "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
